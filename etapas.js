4221981be4a031c9f0b4753e924382fa94e9c24f
Gustavo Guimarães <GGustavoGG@users.noreply.github.com>
2020-11-27 11:28:49 -0300

    lista

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: "Tea Gardner",
+                partido: "CDC",
+                fotos: [
+                    {url: "foto8.jpg", legenda: "Vereador", small: false}
+                ]
+            },
+            {
+                numero: 72222,
+                name: "joey Rian",
+                partido: "MBM",
+                fotos: [
+                    {url: "foto6.jpg", legenda: "Vereador", small: false}
+                ]
+            },
+            {
+                numero: 66699,
+                name: "Seto Kaiba",
+                partido: "MFDP",
+                fotos: [
+                    {url: "foto5.jpg", legenda: "Vereador", small: false}
+                ]
+            },
+            {
+                numero: 66611,
+                name: "Mai Valentine",
+                partido: "MFDP",
+                fotos: [
+                    {url: "foto7.jpg", legenda: "Vereador", small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos:[
+            {
+                numero: 38,
+                name: "Yugi Moto",
+                partido: "CDC",
+                vice: 'Rio Bakura',
+                fotos: [
+                    {url: "foto2.jpg", legenda: "Prefeito", small: false},
+                    {url: "foto1.jpg", legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+            {
+                numero: 45,
+                name: "Jaden Yuki",
+                partido: "PDL",
+                vice: 'Jesse Andersen',
+                fotos: [
+                    {url: "foto3.jpg", legenda: "Prefeito", small: false},
+                    {url: "foto4.jpg", legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+        ]
+    }
+]
